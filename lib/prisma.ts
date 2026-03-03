f007043ce7ef4ab931d5588614c397e64e6623dd
Abdullah Al Mahmood <abdullahalmahmood500@gmail.com>
2026-03-03 21:55:47 +0600

    added connection with neon db postgres

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,10 +1,21 @@
-import "dotenv/config";
-import { PrismaPg } from '@prisma/adapter-pg'
+// import "dotenv/config";
+// import { PrismaPg } from '@prisma/adapter-pg'
+// import { PrismaClient } from '../generated/prisma/client'
+
+// const connectionString = `${process.env.DATABASE_URL}`
+
+// const adapter = new PrismaPg({ connectionString })
+// const prisma = new PrismaClient({ adapter })
+
+// export { prisma }
+
+
+import 'dotenv/config'
 import { PrismaClient } from '../generated/prisma/client'
+import { PrismaNeon } from '@prisma/adapter-neon'
 
-const connectionString = `${process.env.DATABASE_URL}`
+const adapter = new PrismaNeon({
+  connectionString: process.env.DATABASE_URL!,
+})
 
-const adapter = new PrismaPg({ connectionString })
-const prisma = new PrismaClient({ adapter })
-
-export { prisma }
+export const prisma = new PrismaClient({ adapter })
